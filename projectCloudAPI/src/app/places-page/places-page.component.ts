2e1e48fa10d1724ef0fd6f7978ca2fee3e1b53d2
Jonas Van Hoof <jonas.vanhoof@outlook.com>
2019-03-22 18:30:11 +0100

    Api request get array of addresses

--- projectCloudAPI/src/app/places-page/places-page.component.ts
+++ projectCloudAPI/src/app/places-page/places-page.component.ts
@@ -10,15 +10,19 @@ import { Venue } from 'interfacePlace';
 })
 export class PlacesPageComponent implements OnInit {
 places :Venue[] = [];
-streets :Venue[] = [];
+addresses :string[] = [];
 
   constructor(private api: ApiService) {
     this.api.getplaces().subscribe((results => {
       console.log(results);
       this.places = results.response.venues;
+      for(let i = 0; i < this.places.length; i++)
+      this.addresses.push(this.places[i].location.address);
+        console.log(this.addresses);
     }));
   }
 
+
   ngOnInit() {}
 
-}Response
+}
