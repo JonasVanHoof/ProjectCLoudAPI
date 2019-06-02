6246dac0f47f1d9c502729bc6f4ee5dd9fb16fa9
Jonas Van Hoof <jonas.vanhoof@outlook.com>
2019-06-02 13:28:40 +0200

    Commented [Authorize]]

--- RestAPI/RestAPI/Controllers/Circus_materialController.cs
+++ RestAPI/RestAPI/Controllers/Circus_materialController.cs
@@ -11,7 +11,7 @@ using RestAPI.Models;
 namespace RestAPI.Controllers
 {
     [Route("api/[controller]")]
-    [Authorize]
+    //[Authorize]
     [ApiController]
     public class Circus_materialController : Controller
     {
