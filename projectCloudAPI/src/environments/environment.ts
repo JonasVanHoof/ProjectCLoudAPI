e919ed0c11d8fe81778979f10b99c00b11ecda75
Jonas Van Hoof <jonas.vanhoof@outlook.com>
2019-06-13 15:49:51 +0200

    ng build

--- projectCloudAPI/src/environments/environment.ts
+++ projectCloudAPI/src/environments/environment.ts
@@ -4,8 +4,8 @@ export const environment = {
   auth: {
     clientID: 'etQXBmxlniSbbwBkQRRVjF2N0Hjxe7yX',
     domain: 'jonasvanhoof.eu.auth0.com',
-    audience: 'http://jonasvanhoof.me/circusMaterials',
-    redirect: 'http://jonasvanhoof.me/callback',
+    audience: 'https://jonasvanhoof.me/circusMaterials',
+    redirect: 'https://jonasvanhoof.me/callback',
     scope: 'openid profile email'
   }
 };
